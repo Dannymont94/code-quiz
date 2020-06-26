e1f32ccf6d487601b98931911cb27399e65b0848
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-26 16:16:54 -0400

    pseudocoded project

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// create timer variable that starts at 75 and score variable that starts at 0
+
+// create question and answer choice array
+
+// when start button is clicked, start decrementing counter once per second and ask user questions. 
+
+// display questions as text and answer choices as buttons
+
+// determine if user selected correct answer
+
+    // if correct, let user know they answered correctly and increment score. Feedback message goes away after a few seconds.
+
+    // if wrong, let user know they answered incorrectly and decrease timer by 10. Feedback message goes away after a few seconds.
+
+// when timer reaches 0 or end of question array is reached, the game is over. Display user's final score and ask for their initials.
+
+// store final score and initials in localStorage
+
+// display top 5 high scores. Ask user if they want to clear scores or go back to quiz start page.
